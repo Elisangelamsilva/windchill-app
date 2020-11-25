fc163f066cee9315922551098dfbc333d887ee62
Elisangela <elisangela00silva@gmail.com>
2020-11-25 15:59:45 -0300

    adição de verificação de numeros nulos

--- app/controllers/inicial_controller.rb
+++ app/controllers/inicial_controller.rb
@@ -2,6 +2,8 @@ class InicialController < ApplicationController
   def index
   end
   def calculo
-    @resultado = 33 + (10 * Math.sqrt(params[:wind_speed].to_i) + 10.45 - params[:wind_speed].to_i) * (params[:temperature].to_i - 33)/22
+    if params[:wind_speed].present? && params[:temperature].present?
+      @resultado = 33 + (10 * Math.sqrt(params[:wind_speed].to_i) + 10.45 - params[:wind_speed].to_i) * (params[:temperature].to_i - 33)/22
+    end
   end
 end
